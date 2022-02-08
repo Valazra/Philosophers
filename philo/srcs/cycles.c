7a9adb65e50ed4ba446d6484b3d9932aa984778f
Azra <valentin.azra@gmail.com>
2022-02-08 02:41:59 +0100

    cycles

--- philo/srcs/cycles.c
+++ philo/srcs/cycles.c
@@ -6,7 +6,7 @@
 /*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 09:57:14 by user42            #+#    #+#             */
-/*   Updated: 2022/02/07 11:01:29 by user42           ###   ########.fr       */
+/*   Updated: 2022/02/08 02:33:09 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int	first_philo_cycle(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->left_fork_mutex);
-	if (!display_action(philo, "has taken a fork", 2))
+	if (!display_action(philo, "has taken a fork", OTHER))
 	{
 		pthread_mutex_unlock(&philo->left_fork_mutex);
 		return (0);
 	}
 	pthread_mutex_lock(philo->right_fork_mutex);
-	if (!display_action(philo, "has taken a fork", 2))
+	if (!display_action(philo, "has taken a fork", OTHER))
 	{
 		pthread_mutex_unlock(&philo->left_fork_mutex);
 		pthread_mutex_unlock(philo->right_fork_mutex);
 		return (0);
 	}
-	if (!display_action(philo, "is eating", 1))
+	if (!display_action(philo, "is eating", EAT))
 	{
 		pthread_mutex_unlock(&philo->left_fork_mutex);
 		pthread_mutex_unlock(philo->right_fork_mutex);
